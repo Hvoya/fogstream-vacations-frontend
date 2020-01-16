19d2e498d428af49cbfc64e47a2511e1802f1363
hvoya <thatsamejack@yandex.ru>
2020-01-16 13:06:12 +1000

    * express added

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-//server.js
 const express = require('express');
+const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 // the __dirname is the current directory from where the script is running
