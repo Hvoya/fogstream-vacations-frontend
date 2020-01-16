3b7fcc06038c4e673e7a9226b1987d61acbf34d3
hvoya <thatsamejack@yandex.ru>
2020-01-16 12:59:03 +1000

    * express added

--- server.js
+++ server.js
@@ -6,7 +6,4 @@ app.use(express.static(path.join(__dirname, 'dist')));
 app.get('/ping', function (req, res) {
   return res.send('pong');
 });
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'dist'));
-});
 app.listen(port);
