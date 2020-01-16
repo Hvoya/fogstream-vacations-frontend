8d199879870940b85f49c91eb7f628328004f6a1
hvoya <thatsamejack@yandex.ru>
2020-01-16 12:51:38 +1000

    * express added

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 console.log('heeeeeeeee');
-app.use(favicon(__dirname + '/build/favicon.ico'));
+app.use(favicon(__dirname + '/dist/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'dist')));
 app.get('/ping', function (req, res) {
   return res.send('pong');
 });
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 app.listen(port);
