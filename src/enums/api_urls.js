f9b653a0d7f4d82ade55ddeb4450b3100e1ca530
hvoya <thatsamejack@yandex.ru>
2020-01-16 13:37:46 +1000

    * express added

--- src/enums/api_urls.js
+++ src/enums/api_urls.js
@@ -1,5 +1,4 @@
-export const baseUrl =
-  process.env.NODE_ENV === 'https://vacation-visualizer.herokuapp.com/api';
+export const baseUrl = 'https://vacation-visualizer.herokuapp.com/api';
 
 export default {
   login: '/auth/login/',
